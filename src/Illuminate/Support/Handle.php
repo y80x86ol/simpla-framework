444983be124cff73a9696a2f3761e61d44b59165
卖小女孩的小火柴 <364810520@qq.com>
2016-04-05 22:22:43 +0800

    简化error逻辑

--- src/Illuminate/Support/Handle.php
+++ src/Illuminate/Support/Handle.php
@@ -10,15 +10,6 @@
 if (!function_exists('error')) {
 
     function error($msg = '发生了一个未知错误，请检查你的程序') {
-        template($msg);
-        die;
-    }
-
-}
-
-if (!function_exists('template')) {
-
-    function template($msg) {
         $html = <<<EOF
             <h3>Simpla
                 </h3
@@ -26,6 +17,8 @@ if (!function_exists('template')) {
                 $msg
             </p>
 EOF;
+        echo $html;
+        die;
     }
 
 }
