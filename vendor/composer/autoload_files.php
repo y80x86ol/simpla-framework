aa81d8383db46b36d0da1d342f805aac9689872f
y80x86ol <364810520@qq.com>
2016-04-12 22:55:14 +0800

    增加数据库连接ORM：notorm

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'dfdfd514933a425cc9419e64794d08d1' => $vendorDir . '/phpfastcache/phpfastcache/src/phpFastCache/phpFastCache.php',
+    '17fd9fef37c97cfdc0c7794299a8423d' => $vendorDir . '/vrana/notorm/NotORM.php',
     'dccdf8df4fd8321b2e9bf872164027d7' => $baseDir . '/src/Illuminate/Bootstrap.php',
     '7504f560aa383661352a85a3465b15b0' => $baseDir . '/src/Illuminate/Route.php',
 );
